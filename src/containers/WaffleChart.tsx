e2eb2abd92171e77f87f44efe649c2ee2b98ad57
Paula <paugmnoz@gmail.com>
2020-01-27 14:47:11 -0500

    svg appended

--- src/containers/WaffleChart.tsx
+++ src/containers/WaffleChart.tsx
@@ -11,11 +11,31 @@ class WaffleChart extends React.Component<waffleProps>{
         super(props)
     }
 
+    data = [
+        { "name": "type 1", "value": 102},
+        { "name": "type 2", "value": 65},
+        { "name": "type 3", "value": 43},
+        { "name": "type 4", "value": 12}
+      ];
+    componentDidMount(){
+        this.createWaffle();
+    }
+
+    createWaffle(){
+        d3.select("#waffle-cont")
+        .append("svg")
+        .attr("width",'700px')
+        .attr("height", '500px')
+        .classed("waffle-svg-cont", true)
+            }
+
     public render() {
         return (
           // Nos basta con un sencillo DIV.
           // Tan sólo queremos su referencia.
-          <div className="waffle-cont" />
+          <div id="waffle-cont" />
         );
       }
 }
+
+export default WaffleChart;
